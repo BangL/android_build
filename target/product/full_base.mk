ea2d30bfb47074ac68d6d615c4290389074a75f1
BangL <henno.rickowski@googlemail.com>
2013-07-22 06:11:41 +0200

    Get rid of VideoEditor
    Change-Id: Icb564f0c44d4ed92abf255920e8cc10196a8315e

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -21,15 +21,7 @@
 
 PRODUCT_PACKAGES := \
     libfwdlockengine \
-    VideoEditor \
     WAPPushManager
-
-PRODUCT_PACKAGES += \
-    libvideoeditor_jni \
-    libvideoeditor_core \
-    libvideoeditor_osal \
-    libvideoeditor_videofilters \
-    libvideoeditorplayer
 
 include $(SRC_TARGET_DIR)/product/emulator.mk
 
